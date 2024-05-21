ef15a12a7e04df72c83197694023244497619ad7
vyxngg <154455687+vyxngg@users.noreply.github.com>
2024-05-22 00:53:36 +0200

    segundoCommitConexionMongoose

---
+++ models/Products.js
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+    title : {type:String , require: true},
+    description : String,
+    price: {type: Number , require: true}
+
+});
+
+export const Product = model('product',ProductSchema);
